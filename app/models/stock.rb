ea5f4e4d2ba7cc84d7046bce4ea2cad26cea5500
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-05-10 16:05:01 +0300

    Add stock client configuration with secret credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+
+  def self.client
+    IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+        secret_token: Rails.application.credentials.iex_client[:secret_token],
+        endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+  end
 end
