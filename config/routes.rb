f639c667f346837d61eb5f5b0cef37736f21a61f
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-05-24 15:35:06 +0300

    Crete search stocks route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get "portfolio", to: "users#portfolio"
+  get "search_stocks", to: "stocks#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
