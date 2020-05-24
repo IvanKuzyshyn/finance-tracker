3ff69c788ed1be2ff48610c38b9f62b4204bba5c
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-05-24 19:03:24 +0300

    Create user tracking stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,17 @@
 class UserStocksController < ApplicationController
+
+  def create
+    stock = Stock.find_already_existing(params[:ticker])
+
+    if stock.blank?
+      stock = Stock.create_new(params[:ticker])
+      stock.save
+    end
+
+    UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "New Stock #{stock.name} has been tracked!"
+
+    redirect_to portfolio_path
+  end
+
 end
