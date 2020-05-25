c9c22f603c4328ea2e74559a2a67782e2c8fa181
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-05-25 12:37:10 +0300

    Set user tracking restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  def stock_already_tracked?(ticker)
+    stock = Stock.find_already_existing(ticker)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def under_stocks_limit?
+    stocks.count < 5
+  end
+
+  def can_track_stocks?(ticker)
+    under_stocks_limit? && !stock_already_tracked?(ticker)
+  end
 end
